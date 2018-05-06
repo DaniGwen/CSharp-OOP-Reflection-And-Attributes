cf14c514ad223292857143a704c0c88346a3ac60
DaniGwen <you@example.com>
2018-05-06 19:00:37 +0300

    updates

---
+++ P03_BarraksWars/Core/CommandInterpriter.cs
@@ -0,0 +1,51 @@
+﻿using _03BarracksFactory.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace P03_BarraksWars.Contracts.Core
+{
+   public class CommandInterpriter : ICommandInterpreter
+    {
+		private IRepository repository;
+		private IUnitFactory unitFactory;
+
+		private IExecutable InterpredCommand(string[] data, string commandName)
+		{
+
+			Assembly assembly = Assembly.GetCallingAssembly();
+
+			Type commandType = assembly
+				.GetTypes()
+				.FirstOrDefault(t => t.Name.ToLower() == commandName + "command");
+
+			if (commandType == null)
+			{
+				throw new ArgumentException("Invalid command");
+			}
+
+			if (!typeof(IExecutable).IsAssignableFrom(commandType))
+			{
+				throw new Exception($"{commandName } is not valid command!");
+			}
+
+			MethodInfo method = typeof(IExecutable).GetMethods().First();
+
+			object[] commandArgs = new object[] { data, this.repository, this.unitFactory };
+			object instance = Activator.CreateInstance(commandType, commandArgs);
+
+			try
+			{
+				string result = (string)method.Invoke(instance, null);
+				return result;
+			}
+			catch (TargetInvocationException e)
+			{
+				throw e.InnerException;
+			}
+
+		}
+	}
+}
---
+++ P03_BarraksWars/Core/Commands/RetireCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _03BarracksFactory.Contracts;
+
+namespace P03_BarraksWars.Contracts.Core.Commands
+{
+	public class RetireCommand : Command
+	{
+		public RetireCommand(string[] data, IRepository repository, IUnitFactory unitFactory) : base(data, repository, unitFactory)
+		{
+		}
+
+		public override string Execute()
+		{
+			string unitType = Data[1];
+			try
+			{
+				this.Repository.RemoveUnit(unitType);
+				return $"{unitType} retired!";
+			}
+			catch (Exception e)
+			{
+				throw new ArgumentException("No such units in repository.", e);
+			}
+		}
+	}
+}
